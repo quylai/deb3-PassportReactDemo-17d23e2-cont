d2a2218c8be547df8d5928ef3c00c1a62762674f
quyl <quylai@gmail.com>
2019-10-30 05:45:06 -0500

    created RegisterForm comp.
    need to do react routing to alternate between register form and login form

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -36,7 +36,7 @@ class LoginForm extends Component {
     }
 
     render() {
-        return (
+        return(
             <div className="row">
                 <div className="col-8 offset-2">
                     <form>
@@ -45,15 +45,14 @@ class LoginForm extends Component {
                             <input type="text" className="form-control" name="username" id="username" onChange={this.handleChange} value={this.state.username}/>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="password">Username</label>
+                            <label htmlFor="password">Password</label>
                             <input type="password" className="form-control" name="password" id="password" onChange={this.handleChange} value={this.state.password}/>
                         </div>
                         <button type="submit" className="btn btn-primary" onClick={this.loginSubmit}> Submit </button>
                     </form>
                 </div>
             </div>
-    )
-        ;
+        );
     }
 }
 
---
+++ client/src/components/RegisterForm/index.js
@@ -0,0 +1,100 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class RegisterForm extends Component {
+    state = {
+        username: '',
+        password: '',
+        email: '',
+        first_name: '',
+        last_name: ''
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+    loginSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            "username": event.currentTarget.username,
+            "password": event.currentTarget.password,
+            "email": event.currentTarget.password,
+            "first_name": event.currentTarget.password,
+            "last_name": event.currentTarget.password
+        });
+        axios.post('/api/users', {
+            'username': this.state.username,
+            'password': this.state.password
+        }).then((response) => {
+            console.log(response);  //t
+        });
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({[name]: value});
+    }
+
+    render() {
+        return(
+					<div className="row">
+						<div className="col-8 offset-2">
+							<form>
+
+								<div className="form-group">
+									<label htmlFor="username">Username</label>
+									<input type="text" className="form-control" name="username" 
+													id="username" onChange={this.handleChange} 
+													value={this.state.username}
+									/>
+								</div>
+
+								<div className="form-group">
+									<label htmlFor="password">Password</label>
+									<input type="password" className="form-control" name="password" 
+													id="password" onChange={this.handleChange}
+													value={this.state.password}
+									/>
+								</div>
+
+								{/* ---- */}
+
+								<div className="form-group">
+									<label htmlFor="email">Email</label>
+									<input type="text" className="form-control" name="email" 
+													id="email" onChange={this.handleChange} 
+													value={this.state.email}
+									/>
+								</div>
+
+								<div className="form-group">
+									<label htmlFor="first_name">First Name</label>
+									<input type="text" className="form-control" name="first_name" 
+													id="first_name" onChange={this.handleChange} 
+													value={this.state.first_name}
+									/>
+								</div>
+
+								<div className="form-group">
+									<label htmlFor="last_name">Last Name</label>
+									<input type="text" className="form-control" name="last_name" 
+													id="last_name" onChange={this.handleChange} 
+													value={this.state.last_name}
+									/>
+								</div>
+
+								<button 
+									type="submit" className="btn btn-primary" 
+									onClick={this.loginSubmit}>
+										Submit 
+								</button>
+
+							</form>
+						</div>
+					</div>
+        );
+    }
+}
+
+export default RegisterForm;
